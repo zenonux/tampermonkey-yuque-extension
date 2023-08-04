4542d112822bce12ff6ec1387a675555c5b83869
ZenoQi <zenonux@outlook.com>
2023-08-04 15:58:03 +0800

    fix: update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         yuque-html-exporter
 // @namespace    https://github.com/zenonux
-// @version      1.0
+// @version      1.1
 // @description  export yuque document to html.
 // @author       换个头像心好累
 // @license      GPL-3.0 License
